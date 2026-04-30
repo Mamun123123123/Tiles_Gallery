6a236b0e6528191ed31d8b6330d8eb2effe9350f
Mamun123123123 <almalm90459@gmail.com>
2026-04-30 19:40:54 +0600

    Home section

---
+++ src/components/Banner.jsx
@@ -0,0 +1,41 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Banner = () => {
+  return (
+     <div
+      className="h-[60vh] w-full bg-cover bg-center bg-no-repeat rounded-lg shadow-2xl"
+      style={{
+        backgroundImage:
+          "url('https://thumbs.dreamstime.com/b/d-seamless-ceramic-wall-tiles-design-texture-wallpaper-design-pattern-graphics-design-art-background-ceramic-floor-tiles-366053902.jpg')",
+      }}
+    >
+     
+      <div className="w-full h-full  bg-black/60 rounded-lg flex items-center">
+        <div className="max-w-7xl mx-auto px-6 text-white">
+
+          <h1 className="text-4xl md:text-5xl font-bold mb-4 max-w-2xl">
+            Discover Your Perfect Aesthetic Tiles
+          </h1>
+
+      
+          <p className="text-lg md:text-xl mb-6 max-w-xl text-gray-200">
+            Explore premium tile designs and transform your interior spaces with elegance.
+          </p>
+
+          <div className="flex gap-4 items-center justify-center flex-wrap">
+
+            <Link href="/all-tiles">
+              <button className="px-6 py-3 rounded-lg font-medium bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500 hover:opacity-90 transition">
+                Browse Now
+              </button>
+            </Link>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Banner
