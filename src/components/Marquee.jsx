64f48201d9b630cba073b33280d376f04b47f4ef
Mamun123123123 <almalm90459@gmail.com>
2026-04-30 19:49:25 +0600

    Marquee section

---
+++ src/components/Marquee.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Marquee = () => {
+  return (
+       <div className="bg-base-200 py-3 rounded-lg overflow-hidden mt-6">
+      <marquee className="text-sm font-medium text-gray-700 dark:text-gray-300">
+         New Arrivals: Ceramic Blue Tile |  Marble Elegance Tile |  Weekly Feature: Modern Geometric Patterns |  Join the Community |  Premium Interior Designs for Modern Homes
+      </marquee>
+    </div>
+  )
+}
+
+export default Marquee
