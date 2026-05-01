40594a4cb78b47bdc597135e1e84972f9f140d2b
Mamun123123123 <almalm90459@gmail.com>
2026-05-01 11:01:27 +0600

    change layout design

--- src/app/layout.js
+++ src/app/layout.js
@@ -28,7 +28,7 @@ export default function RootLayout({ children }) {
     >
       <body className="min-h-full flex flex-col">
         <Navbar />
-        <main className="bg-slate-200 p-6">
+        <main className="bg-slate-200 p-2 sm:p-6">
           {children}
         </main>
         
